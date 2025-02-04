12ae3c6732bfe9dd3c491843f5a7ce9e2b3e99da
Walter do Valle <walter.valle@gmail.com>
2025-02-04 19:51:06 -0300

    Criação do projeto

---
+++ src/main/java/me/dio/devweek/DevweekApplication.java
@@ -0,0 +1,13 @@
+package me.dio.devweek;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DevweekApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DevweekApplication.class, args);
+	}
+
+}
