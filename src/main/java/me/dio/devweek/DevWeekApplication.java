ba29beed11c52966469053b730195db4ec09a913
Walter do Valle <walter.valle@gmail.com>
2025-02-04 22:53:42 -0300

    Typo

--- src/main/java/me/dio/devweek/DevWeekApplication.java
+++ src/main/java/me/dio/devweek/DevWeekApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class DevweekApplication {
+public class DevWeekApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(DevweekApplication.class, args);
+		SpringApplication.run(DevWeekApplication.class, args);
 	}
 
 }
